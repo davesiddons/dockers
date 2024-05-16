1478f4322f744035c041813183a7df5f128269ee
Dave Siddons <david.siddons@engineering.digital.dwp.gov.uk>
2024-05-16 19:39:24 +0100

    new versions

--- hadoop_hive_spark_docker/build.sh
+++ hadoop_hive_spark_docker/build.sh
@@ -4,25 +4,25 @@
 echo "Y" | ssh-keygen -t rsa -P "" -f configs/id_rsa
 
 # Hadoop build
-docker build -f ./hadoop/Dockerfile . -t sciencepal/hadoop_cluster:hadoop
+docker build -f ./hadoop/Dockerfile . -t siddo/hadoop
 
 # Spark
-docker build -f ./spark/Dockerfile . -t sciencepal/hadoop_cluster:spark
+docker build -f ./spark/Dockerfile . -t siddo/hadoop
 
 # PostgreSQL Hive Metastore Server
-docker build -f ./postgresql-hms/Dockerfile . -t sciencepal/hadoop_cluster:postgresql-hms
+docker build -f ./postgresql-hms/Dockerfile . -t siddo/hadoop
 
 # Hive
-docker build -f ./hive/Dockerfile . -t sciencepal/hadoop_cluster:hive
+docker build -f ./hive/Dockerfile . -t siddo/hadoop
 
 # Nifi
-docker build -f ./nifi/Dockerfile . -t sciencepal/hadoop_cluster:nifi
+docker build -f ./nifi/Dockerfile . -t siddo/hadoop
 
 # Edge
-docker build -f ./edge/Dockerfile . -t sciencepal/hadoop_cluster:edge
+docker build -f ./edge/Dockerfile . -t siddo/hadoop
 
 # hue
-docker build -f ./hue/Dockerfile . -t sciencepal/hadoop_cluster:hue
+docker build -f ./hue/Dockerfile . -t siddo/hadoop
 
 # zeppelin
-docker build -f ./zeppelin/Dockerfile . -t sciencepal/hadoop_cluster:zeppelin
+#docker build -f ./zeppelin/Dockerfile . -t siddo/hadoop
